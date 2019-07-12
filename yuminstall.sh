cb2a362b149f31cc5315282a8850774743ca298a
sangminbanghak <52808640+sangminbanghak@users.noreply.github.com>
2019-07-12 12:02:18 +0900

    Add files via upload

---
+++ yuminstall.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+source /home/work
+mkdir /home/work
+echo "work directory is ready!"
+sleep 2
+cd /home/work
+echo "now" 'pwd'
+sleep 2
+yum install -y cmake *gcc* cvs telnet ypbind compat*
+sleep 1
+yum install -y glibc.i686 glibc*
+sleep 1
+yum install -y redhat-lsb-core libcurl* libxslt*
+sleep 1
+yum install -y pcre*
+sleep 1
+yum install -y libz* bzip2-devel* readline-devel*
+echo "default rpm installed"
